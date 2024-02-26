9e15f531444e4381729db817d4eefa4af0fa17dc
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-26 12:24:12 +0200

    Rought draft placeholder of the future logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,15 @@ import Holidays from "@/components/Holidays";
 
 const Home = () => (
   <>
+    <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+      Weekly view
+    </button>
+    |
+    <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Monthly view
+    </button>
+    <hr />
+    Calendar UI
     <Holidays />
   </>
 );
