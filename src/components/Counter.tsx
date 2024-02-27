bcbd755d8625d9021862887bf441472a10ab77cf
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-27 16:25:29 +0200

    Implement Prisma increment button

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,19 +1,37 @@
+import { redirect } from "next/navigation";
 import React from "react";
 
 import prisma from "@/helpers/db";
 
+async function createTodo(_data: FormData) {
+  "use server";
+
+  await prisma.counter.update({
+    where: { id: 1 },
+    data: { count: { increment: 1 } },
+  });
+
+  redirect("/");
+}
+
 const Counter = async () => {
   const counter = await prisma.counter.upsert({
-    update: { count: { increment: 1 } },
+    update: {},
     create: { count: 1 },
     where: { id: 1 },
   });
 
   return (
     <div className="shadow-md p-4 m-4">
-      Counter value is {counter.count}
-      <hr />
-      {/* increment form */}
+      <div>Counter value is {counter.count}</div>
+
+      <hr className="my-4" />
+
+      <form action={createTodo}>
+        <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+          Increment
+        </button>
+      </form>
     </div>
   );
 };
