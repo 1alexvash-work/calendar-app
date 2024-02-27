57a2f9a209016e5efdbba8fac7d55930cdccf73c
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-27 12:19:08 +0200

    Refactor Counter to have served code inside the component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,22 +2,19 @@ import React from "react";
 
 import prisma from "@/helpers/db";
 
-const getCounter = async () => {
+const Counter = async () => {
   const counter = await prisma.counter.upsert({
     update: { count: { increment: 1 } },
     create: { count: 1 },
     where: { id: 1 },
   });
-  console.log("counter:", counter);
-
-  return counter;
-};
-
-const Counter = async () => {
-  const counter = await getCounter();
 
   return (
-    <div className="shadow-md p-4 m-4">Counter value is {counter.count}</div>
+    <div className="shadow-md p-4 m-4">
+      Counter value is {counter.count}
+      <hr />
+      {/* increment form */}
+    </div>
   );
 };
 
