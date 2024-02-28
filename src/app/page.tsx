59b7f7f37c24c794db89b546527e10e7bbb50787
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-28 09:26:36 +0200

    Remove weekly/monthly toggles since I am not likely to implement them in this scope of work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,14 +31,6 @@ const Home = async () => {
 
   return (
     <>
-      <button className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-        Weekly view
-      </button>
-      |
-      <button className="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Monthly view
-      </button>
-      <hr />
       <CalendarUI holidays={holidays} />
       <Counter />
     </>
